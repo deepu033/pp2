f1cde8a92e4a4653e33e123bc692aed603344965
karan <purexkaran@gmail.com>
2026-01-07 13:20:50 +0530

    BACKGROUND ONLY

--- script.js
+++ script.js
@@ -809,4 +809,3 @@ function formatDate(dateString) {
     const options = { day: 'numeric', month: 'short', year: 'numeric' };
     return new Date(dateString).toLocaleDateString('en-US', options);
 }
-
